ae2e6d0e6141390fa96d765d6f5df8e7999f7b20
hcharon <hcharon@softia.fr>
2025-08-24 23:39:05 +0200

    refacto auth for front

--- chat-service-brack/src/main/java/com/yourcaryourway/chat/chat_service/controllers/AuthenticationController.java
+++ chat-service-brack/src/main/java/com/yourcaryourway/chat/chat_service/controllers/AuthenticationController.java
@@ -1,6 +1,5 @@
 package com.yourcaryourway.chat.chat_service.controllers;
 
-
 import com.yourcaryourway.chat.chat_service.dtos.user.LoginUserDto;
 import com.yourcaryourway.chat.chat_service.dtos.user.RegisterUserDto;
 import com.yourcaryourway.chat.chat_service.models.User;
@@ -9,12 +8,16 @@ import com.yourcaryourway.chat.chat_service.services.AuthenticationService;
 import com.yourcaryourway.chat.chat_service.services.JwtService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RequestMapping("/auth")
-//@CrossOrigin(origins = "http://localhost:4200")  // Autoriser l'accès depuis Angular
+//@CrossOrigin(origins = "http://localhost:4200")  // Autoriser l'accès depuis Angular si besoin
 @RestController
 public class AuthenticationController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
+
     private final JwtService jwtService;
     private final AuthenticationService authenticationService;
 
@@ -23,13 +26,12 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
-
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> authenticate(
-            @RequestBody LoginUserDto loginUserDto) {
-
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
         User authenticatedUser = authenticationService.authenticate(loginUserDto);
         String jwtToken = jwtService.generateToken(authenticatedUser);
+        System.out.println("Info utilisateur: " + authenticatedUser);
+        jwtService.debugToken(jwtToken);
         LoginResponse loginResponse = new LoginResponse()
                 .setToken(jwtToken);
 
